06ed832fe4cc90ca996aacc403313bae0ca87c7d
AleixMT <aleix.marine@estudiants.urv.cat>
2020-10-05 19:17:20 +0200

    changes

---
+++ battleship.c
@@ -0,0 +1,7 @@
+#include "c_script.h"
+  int main(int nargs, char* args[])
+{
+  printf("Hello World");
+}
+
+
---
+++ types.h
@@ -0,0 +1,5 @@
+// Includes
+#include <stdio.h>
+#include <stdbool.h>  // To use booleans
+#include <stdlib.h>
+
