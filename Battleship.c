e0200d675d16f0300ad850628c3dd14df8fb4f33
axel <axelfernandezcurros@gmail.com>
2020-10-10 01:28:25 +0200

    changes

--- Battleship.c
+++ Battleship.c
@@ -1,8 +1,27 @@
 #include "Battleship.h"
-
+/**
+ * Prints the board given by parameter.
+ * @param board
+ */
+void showBoard(char* board)
+{
+	printf("\n ");
+	for (unsigned int i = 0; i < DIM; i++)
+	{
+		printf("%c", 'A' + i);
+	}
+	for (unsigned int i = 0; i < DIM; i++)
+	{	
+		printf("%i", 
+	
+		for (unsigned int j = 0; j < DIM; j++)
+		{
+			
+		}
+	}
+}
 int main(int nargs, char* args[])
 {
-  printf("Hello World");
+	printf("HELLO");
 }
-
 
